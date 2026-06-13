c717ca3fc35d58fea36398f1060aa59c0a171a7f
ばなな(choco) <kuru-api@hotmail.com>
2026-06-13 20:00:15 +0900

    Add gameLoadingStart to PokiSDK mock

--- plactions/patch/poki-sdk-core-v2.263.0.js
+++ plactions/patch/poki-sdk-core-v2.263.0.js
@@ -1,6 +1,7 @@
 // Poki SDK Core - mock implementation (no tracking)
   window.PokiSDK = {
     init: function() { return Promise.resolve(); },
+    gameLoadingStart: function() {},
     gameLoadingFinished: function() {},
     gameplayStart: function() {},
     gameplayStop: function() {},
